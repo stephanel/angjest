dea6a371a9dd2fb12a0b0d81ea2dc43674071e61
Stéphane Lasmartres <slasmartres@gmail.com>
2020-04-18 11:28:01 -1000

    add eslint/prettier

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,31 +4,29 @@ jest.mock('child_process');
 // import * as child from 'child_process';
 
 describe('AngJest', () => {
-    
     it('should setup angular project', () => {
-        
-        // expect(exec).toHaveBeenCalledWith('npm -v');
+        /*
+        expect(exec).toHaveBeenCalledWith('npm -v');
 
-        // expect node run 'npm install -D jest jest-preset-angular @types/jest'
-        // expect node run 'npm install -D jest jest-preset-angular @types/jest'
+        expect node run 'npm install -D jest jest-preset-angular @types/jest'
+        expect node run 'npm install -D jest jest-preset-angular @types/jest'
 
-        // expect node create file  './setupJest.ts'
+        expect node create file  './setupJest.ts'
 
-        // expect node add Jest configuration to package.json file
+        expect node add Jest configuration to package.json file
 
-        // expect node adjust "compilerOptions.types" section in tsconfig.spec.json
+        expect node adjust "compilerOptions.types" section in tsconfig.spec.json
 
-        // expect node adjust "scripts" section in package.json
+        expect node adjust "scripts" section in package.json
 
-        // expect node remove "projects.[your-project].architect.test" section in angular.json
+        expect node remove "projects.[your-project].architect.test" section in angular.json
 
-        // expect node run 'npm remove karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-reporter'
+        expect node run 'npm remove karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-reporter'
 
-        // expect node run 'npm remove jasmine @types/jasmine @types/jasminewd2 jasmine-core jasmine-spec-reporter'
-        // if user asked for
+        expect node run 'npm remove jasmine @types/jasmine @types/jasminewd2 jasmine-core jasmine-spec-reporter'
+        if user asked for
 
-        // expect node has removed file './karma.conf.js'
-
+        expect node has removed file './karma.conf.js'
+        */
     });
-    
 });
