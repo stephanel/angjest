9672bd7690b94303334d5131b7fbb4f7c5494236
Stéphane Lasmartres <SLasmartres@idt.pf>
2020-04-17 18:45:27 -1000

    config eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-const value = 'Heeelo FizzBuzz';
+import { exec } from 'child_process';
 
-console.log(value);
+exec('npm -v');
+
+function a(id:number,name:string,mail_id?:string) { 
+    console.log("ID:", id); 
+    console.log("Name",name); 
+    
+    if(mail_id!=undefined)  
+    console.log("Email Id",mail_id); 
+ }
---
+++ test/index.spec.ts
@@ -0,0 +1,34 @@
+jest.mock('child_process');
+
+// import { exec } from 'child_process';
+// import * as child from 'child_process';
+
+describe('AngJest', () => {
+    
+    it('should setup angular project', () => {
+        
+        // expect(exec).toHaveBeenCalledWith('npm -v');
+
+        // expect node run 'npm install -D jest jest-preset-angular @types/jest'
+        // expect node run 'npm install -D jest jest-preset-angular @types/jest'
+
+        // expect node create file  './setupJest.ts'
+
+        // expect node add Jest configuration to package.json file
+
+        // expect node adjust "compilerOptions.types" section in tsconfig.spec.json
+
+        // expect node adjust "scripts" section in package.json
+
+        // expect node remove "projects.[your-project].architect.test" section in angular.json
+
+        // expect node run 'npm remove karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-reporter'
+
+        // expect node run 'npm remove jasmine @types/jasmine @types/jasminewd2 jasmine-core jasmine-spec-reporter'
+        // if user asked for
+
+        // expect node has removed file './karma.conf.js'
+
+    });
+    
+});
